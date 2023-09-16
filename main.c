4448811032b26b0e5c076361c563650aea5513a3
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 14:13:19 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+// Program to find the min of two values using macros
+
 #include <stdio.h>
 
+#define MIN(x, y) x > y ? y : x
+
 int main(void) {
-  printf("Hello World\n");
+
+  int x = 4, y = 8;
+  printf ("The minimum of %d and %d is %d\n", x, y, MIN (x, y));
+  
   return 0;
 }
